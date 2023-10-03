2dcde06f86bb89c2d0330a541eabf45f68752971
ArmanNeisi <146854818+ArmanNeisi@users.noreply.github.com>
2023-10-03 17:52:20 +0200

    Update 01.beginning.R

--- 01.beginning.R
+++ 01.beginning.R
@@ -14,3 +14,20 @@ final <- Zuma * duccio
 final
 
 final^2
+
+#array
+sophi <- c(10, 20, 30, 50, 70)  #microplastics
+#functions have parentheses and inside them there are argument
+
+Paula <- c(100, 500, 600, 1000, 2000)  #people
+
+plot(paula, sophi)
+
+plot(paula, sophi, xlab="number of people", ylab="organisms")
+یا
+people<- sophi
+organisms<-paula
+plot(people, organisms)
+
+plot(people, organisms, pch=19, cex=12)
+plot(people, organisms, pch=19, cex=12, col="blue")
