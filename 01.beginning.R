9bebc100479192df9c79a161554e9e902fef8dfc
ArmanNeisi <146854818+ArmanNeisi@users.noreply.github.com>
2023-10-03 17:04:08 +0200

    Create 01.beginning.R

---
+++ 01.beginning.R
@@ -0,0 +1,16 @@
+# Here I can write anything I want!! 42 is the meaning of life universe and all!
+
+#R as a calculator
+2  + 3
+
+#assign to an abject
+Zuma <- 2 + 3
+zima
+
+duccio <- 5 + 3
+duccio
+
+final <- Zuma * duccio
+final
+
+final^2
