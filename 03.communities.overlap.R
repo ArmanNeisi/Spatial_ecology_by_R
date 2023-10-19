67c2652c9af2cced1c2e19778eeb86a8861861fa
ArmanNeisi <146854818+ArmanNeisi@users.noreply.github.com>
2023-10-19 17:42:18 +0200

    Update 03.communities.overlap.R

--- 03.communities.overlap.R
+++ 03.communities.overlap.R
@@ -1,1 +1,33 @@
+# 19 oct جزوه
 
+#relation among species in time:
+
+install.packages("overlap")
+library(overlap)
+
+data(kerinci)
+
+head(kerinci)
+kerinci
+summary(kerinci)
+
+#selecting a species
+
+tiger <- kerinci[kerinci$sps == "tiger", ]
+
+kerinci$timeRad <- kerinci$time * 2 * pi
+timeRad
+
+#selecting the time for the tiger
+timetig <- tiger$timeRad
+densityplot(timetig,  rug=True)
+
+#exercise: select only data on macaque individuals
+
+macaque <- kerinci[kerinci$sps == "macaque", ]
+
+kerinci$timeRad <- kerinci$time * 2 * pi
+timeRad
+
+timetig <- macaque$timeRad
+densityplot(timetig,  rug=True)
