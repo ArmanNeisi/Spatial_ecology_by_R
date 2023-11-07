573bdb8d245b4a250d8ac24f9873eba8c4033330
ArmanNeisi <146854818+ArmanNeisi@users.noreply.github.com>
2023-11-07 17:47:05 +0100

    Update 05.specteral.indices.r

--- 05.specteral.indices.r
+++ 05.specteral.indices.r
@@ -2,3 +2,11 @@
 
 library(imageRy)
 library(terra)
+
+im.list()
+
+im.import("motogrosso-l5-1992219-lrg.jpg")
+# band: 1= NIR, 2=red, 3=green
+im.plotRGB(m1992, r=1, g=2, b=3)
+im.plotRGB(m1992, r=2, g=1, b=3)
+im.plotRGB(m1992, r=2, g=3, b=1)
