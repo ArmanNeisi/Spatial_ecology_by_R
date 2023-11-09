1849be620abdca1ea12d1c392c913d3ee8ce3200
ArmanNeisi <146854818+ArmanNeisi@users.noreply.github.com>
2023-11-09 16:37:32 +0100

    Update 05.specteral.indices.r

--- 05.specteral.indices.r
+++ 05.specteral.indices.r
@@ -13,6 +13,14 @@ im.plotRGB(m1992, r=2, g=3, b=1)
 
 
 # import recent image
-
-m2006 <- im.import("اسم عکس بالا")
+# عددها را از خود نتایج انتخاب میکنیم 
+m2006 <- im.import("motogrosso-l5-2006209-lrg.jpg")
 im.plotRGB(m2006, r=2, g=3, b=1)
+
+# exercise; build a multiframe with 1992 and 2006 images:
+im.plotRGB(m2006, r=2, g=3, b=1)
+im.plotRGB(m2006, r=2, g=3, b=1)
+par(mfrow= c(1,2))
+
+#mfrow means a multiframe which begins with row
+
