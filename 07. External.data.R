de9d84bee1d29a96534e33d6067ca6ceb80ca5a7
ArmanNeisi <146854818+ArmanNeisi@users.noreply.github.com>
2023-11-16 16:56:04 +0100

    Update 07. External.data.R

--- 07. External.data.R
+++ 07. External.data.R
@@ -1,1 +1,10 @@
+# external data
 
+library(terra)
+
+#set the working directory based on your path: 
+# setwd("yourpath")
+# windows users: c://path/downloads
+# my own:
+setwd("مسیر عکسی که دانلود کردید در کامپیوترتان")
+naji <- rast("نام عکس")  #like in im.import 
