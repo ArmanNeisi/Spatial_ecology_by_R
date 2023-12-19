333f36902f658bbed92514b8b6963a56f485c625
ArmanNeisi <146854818+ArmanNeisi@users.noreply.github.com>
2023-12-19 17:49:57 +0100

    Update 10.variabiliy.R

--- 10.variabiliy.R
+++ 10.variabiliy.R
@@ -1,1 +1,28 @@
+# measurement of RS based variability 
 
+library(imageRy)
+library(terra)
+library(viridis)
+
+im.list()
+
+sent <- im.import("sentinel.png")
+
+#band 1 = NIR
+# band 2 = red
+# band 3 = green
+
+im.plotRGB(sent, r=1, g=2, b=3)
+im.plotRGB(sent, r=1, g=2, b=3)
+
+NIR <- sent[[1]]
+plot(NIR)
+
+
+#moving window
+# focal function 
+sd3 <- focal(NIR, matrix(1/9, 3, 3), fun= sd) # matrix describes the dimension of our moving windows
+plot(sd3) #standard variation
+
+colorRampPalette(viridis(7))(255)
+plot(sd3, col= viridis)
