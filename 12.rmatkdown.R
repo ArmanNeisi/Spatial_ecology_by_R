a110db1ae56462aa2969493cd0a3f3952b243cd5
ArmanNeisi <146854818+ArmanNeisi@users.noreply.github.com>
2024-01-09 17:40:19 +0100

    Create 12.rmatkdown.R

---
+++ 12.rmatkdown.R
@@ -0,0 +1,21 @@
+---
+title: "test"
+author: "Duccio Rocchini"
+date: "2024-01-08"
+output: html_document
+---
+# بالایی را از r کپی کردیم.
+
+
+# my first markdown document!
+
+here you can add some text if you want!
+
+in this case you attain 
+```{r, eval=T}
+2 + 3
+```
+in this case you do not attain
+```{r, eval=F}
+2 + 3
+```
